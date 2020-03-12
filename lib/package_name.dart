a6d22e4fb2c8de85fc1587b6c0953398d6a1cb9c
Dushyant <Dushyant.K.Bhattu@gmail..com>
2020-03-12 13:39:34 +0530

    version 2

--- lib/package_name.dart
+++ lib/package_name.dart
@@ -3,5 +3,7 @@ library package_name;
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+  int addOne(int value) => value + 1; // V1
+
+  int addTwo(int value) => value + 2; // V2
 }
